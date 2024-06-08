bb8418bb54f5c8808d1f98dc5ce7518c3bb25bc8
Praseesh <praseeshprasee@gmail.com>
2024-06-09 01:45:13 +0530

    search added

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -4,4 +4,8 @@ from .models import Product
 
 def index(request):
     product_objects = Product.objects.all()
+    
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains = item_name)
     return render(request, 'shop/index.html',{'product_objects':product_objects})
