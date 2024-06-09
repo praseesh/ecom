08506b4188b00420a28d0f036cd103ffa955e431
Praseesh <praseeshprasee@gmail.com>
2024-06-09 20:44:12 +0530

    cart

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from shop import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('<int:id>/', views.detail, name="detail"),
 ]
--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -14,3 +14,7 @@ def index(request):
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
     return render(request, 'shops/index.html',{'product_objects':product_objects})
+
+def detail(request,id):
+    product_object = Product.objects.get(id=id)
+    return render(request,'shops/detail.html', {'product_object':product_object})
