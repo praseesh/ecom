fcf193875ff35b519a67091b8bf699ccf4ff0903
Praseesh <praseeshprasee@gmail.com>
2024-06-09 22:28:45 +0530

    cart

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -8,7 +8,6 @@ def index(request):
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
         product_objects = product_objects.filter(title__icontains = item_name)
-        
     # paginator code
     paginator = Paginator(product_objects,4)
     page = request.GET.get('page')
